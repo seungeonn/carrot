8e43280a7ee593696c2e770331fd9941a13fd271
seungeonn <2lee3145@naver.com>
2023-12-12 13:50:38 +0900

    login

--- app/detail/[id]/page.js
+++ app/detail/[id]/page.js
@@ -5,6 +5,7 @@ import { ObjectId } from "mongodb";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/pages/api/auth/[...nextauth]";
 import Comment from './Comment'
+import Login from "@/app/Login";
 
 export default async function Detail(props) {
 
@@ -21,52 +22,55 @@ export default async function Detail(props) {
 
 
   return (
+    session ? 
     <div className="detail">
 
-      {
-        writerId == loginId ?
-        <div className="userSelf">
-          <Link href={'/edit/'+ props.params.id}>수정</Link>
-          <form action={"/api/post/delete?postId=" + props.params.id} method="POST"><button type="submit">삭제</button></form>
-        </div>
-      : null
-      }
-      
-      {
-          <div className="box">
-            <div className="imgBox">
-              {
-                JSON.parse(result.imgUrl).map((a,i)=>{
-                  return(
-                    <div className="img"><img src={a} alt={i} /></div>
-                  )
-                })
-              }
-            </div>
-            <div className="text">
-              <div className="userInformation">
-                <div className="userName">{result.nickname}<span> 님의 판매</span></div>
-                <div className="userEmail">{result.email}</div>
-              </div>
-              <div className="contentBox">
-                <div className="title">{result.title}</div>
-                <div className="content">{result.content}</div>
-              </div>
-                <div className="priceBox">
-                  <div className="price">{result.price}원</div>
-                  {/* <form action={"/api/chat/chatBtn?postId=" + props.params.id + '&loginId=' + loginId + '&writerId=' + writerId} method="POST">
-                    {
-                      session ? session.user._id == writerId ? null :
-                      <button className="chatBtn" type="submit">채팅하기</button> : null
-                    }
-                  </form> */}
-                </div>
-            </div>
+    {
+      writerId == loginId ?
+      <div className="userSelf">
+        <Link href={'/edit/'+ props.params.id}>수정</Link>
+        <form action={"/api/post/delete?postId=" + props.params.id} method="POST"><button type="submit">삭제</button></form>
+      </div>
+    : null
+    }
+    
+    {
+        <div className="box">
+          <div className="imgBox">
+            {
+              JSON.parse(result.imgUrl).map((a,i)=>{
+                return(
+                  <div className="img"><img src={a} alt={i} /></div>
+                )
+              })
+            }
           </div>
-      }
+          <div className="text">
+            <div className="userInformation">
+              <div className="userName">{result.nickname}<span> 님의 판매</span></div>
+              <div className="userEmail">{result.email}</div>
+            </div>
+            <div className="contentBox">
+              <div className="title">{result.title}</div>
+              <div className="content">{result.content}</div>
+            </div>
+              <div className="priceBox">
+                <div className="price">{result.price}원</div>
+                {/* <form action={"/api/chat/chatBtn?postId=" + props.params.id + '&loginId=' + loginId + '&writerId=' + writerId} method="POST">
+                  {
+                    session ? session.user._id == writerId ? null :
+                    <button className="chatBtn" type="submit">채팅하기</button> : null
+                  }
+                </form> */}
+              </div>
+          </div>
+        </div>
+    }
 
-      <Comment result2={result2} session={session} postId={props.params.id} result={result}></Comment>
+    <Comment result2={result2} session={session} postId={props.params.id} result={result}></Comment>
 
-    </div>
+  </div>
+
+  : <Login></Login>
   )
 }
